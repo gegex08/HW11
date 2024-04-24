c034398571a48d616cd0862eaadd19ac7c7df648
Genny Ocampo <gegex08@aol.com>
2024-04-24 07:44:52 -0500

    Update GradeLevel.java

--- GradeLevel.java
+++ GradeLevel.java
@@ -1,65 +1,20 @@
 package gradeleveltest;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 
-class GradeLevel {
-    int NumWords = 0;
-    int NumSentences = 0;
-    int NumSyllables = 0;
+public class GradeLevelTest {
 
-    public void readFile() throws IOException {
-        BufferedReader inputStream = null;
-        try {
-            inputStream = new BufferedReader(new FileReader("/Users/geneivaocampo/Desktop/File.docx"));
-            String line;
-            while ((line = inputStream.readLine()) != null) {
-                readLine(line);
-            }
-        } finally {
-            if (inputStream != null) {
-                inputStream.close();
-            }
-        }
+        public static void main(String[] args) throws IOException {
+        GradeLevel gradeLevel = new GradeLevel();
+        gradeLevel.readFile();
+
+        int WordsInLine = gradeLevel.getNumWords();
+        int NumLines = gradeLevel.getNumSentences();
+        int syllableCount = gradeLevel.getNumSyllables();
+        int gradeLevelValue = gradeLevel.calculateGradeLevel(WordsInLine, NumLines, syllableCount);
+
+        System.out.printf("Number of Words in all  %d lines are %d\n", NumLines, WordsInLine);
+        System.out.println("Syllable count: " + syllableCount);
+        System.out.println("Grade level: " + gradeLevelValue);
     }
 
-    public void readLine(String line) {
-        String[] sentences = line.split("[.!?] ");
-        NumSentences += sentences.length;
-        for (String sentence : sentences) {
-            String[] words = sentence.trim().split("\\s+");
-            NumWords += words.length;
-            for (String word : words) {
-                NumSyllables += countSyllables(word); // counting syllables
-            }
-        }
-    }
-
-    private int countSyllables(String word) {
-        int syllableCount = 0;
-        for (char token : word.toCharArray()) {
-            syllableCount += String.valueOf(token).length() / 3; // update the syllable count
-        }
-        return syllableCount;
-    }
-
-
-    public int calculateGradeLevel(int totalWords, int totalSentences, int totalSyllables) {
-        double wordsPerSentence = (double) totalWords / totalSentences;
-        int gradeLevel = (int) (0.39 * wordsPerSentence + 11.8 * totalSyllables / totalWords - 15.59);
-        return gradeLevel;
-    }
-
-    public int getNumWords() {
-        return NumWords;
-    }
-
-    public int getNumSentences() {
-        return NumSentences;
-    }
-
-    public int getNumSyllables() {
-        return NumSyllables;
-    }
 }
