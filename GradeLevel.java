df8f4daf284e9f388e15491517e4c44b41dfbf80
Genny Ocampo <gegex08@aol.com>
2024-04-24 07:45:10 -0500

    Update GradeLevel.java

--- GradeLevel.java
+++ GradeLevel.java
@@ -1,20 +1,77 @@
 package gradeleveltest;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-public class GradeLevelTest {
+public class GradeLevel {
+    private int NumLines = 0;
+    private int WordsInLine = 0;
+    private int NumWords = 0;
+    private int NumSentences = 0;
+    private int NumSyllables = 0;
 
-        public static void main(String[] args) throws IOException {
-        GradeLevel gradeLevel = new GradeLevel();
-        gradeLevel.readFile();
+    public void readFile() throws IOException {
+        
 
-        int WordsInLine = gradeLevel.getNumWords();
-        int NumLines = gradeLevel.getNumSentences();
-        int syllableCount = gradeLevel.getNumSyllables();
-        int gradeLevelValue = gradeLevel.calculateGradeLevel(WordsInLine, NumLines, syllableCount);
+        BufferedReader inputStream = null;
+        PrintWriter outputStream = null;
 
-        System.out.printf("Number of Words in all  %d lines are %d\n", NumLines, WordsInLine);
-        System.out.println("Syllable count: " + syllableCount);
-        System.out.println("Grade level: " + gradeLevelValue);
+        try {
+            inputStream = new BufferedReader(new FileReader("/Users/geneivaocampo/Desktop/File.txt"));
+            outputStream = new PrintWriter(new FileWriter("/Users/geneivaocampo/Desktop/File1.txt"));
+            String l;
+            while ((l = inputStream.readLine()) != null) {
+                outputStream.println(l);
+                printNums(l);
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
     }
 
+    
+    private void printNums(String sentence) {
+        NumLines++;
+        String[] sentences = sentence.split("[.!?] ");
+        NumSentences += sentences.length;
+
+        String[] words = sentence.split(" ");
+        NumWords += words.length;
+        
+        for (String word : words) {
+            System.out.println(word);
+            System.out.println("Length: " + word.length());
+            System.out.printf("Syllable: %d\n", word.length() / 3);
+        }
+    }
+
+
+    public int calculateGradeLevel(int totalWords, int totalSentences, int totalSyllables) {
+        double wordsPerSentence = (double) totalWords / totalSentences;
+        int gradeLevel = (int) (0.39 * wordsPerSentence + 11.8 * totalSyllables / totalWords - 15.59);
+        return gradeLevel;
+    }
+
+    
+    
+    
+    public int getNumWords() {
+        return NumWords;
+    }
+
+    public int getNumSentences() {
+        return NumSentences;
+    }
+
+    public int getNumSyllables() {
+        return NumSyllables;
+    }
 }
